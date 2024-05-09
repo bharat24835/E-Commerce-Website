e3a8dcdc2f47cfb86686c41c0c59c76a465e0957
Bharat <bharat24835@gmail.com>
2024-05-09 23:12:50 +0530

    may 9 11pm

---
+++ client/src/components/LayOut/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Footer = ()=>{
+    return (
+        <div className="bg-dark text-light p-3" >
+            <h4 className="text-center">All Rights Reserved &copy; Bharat</h4>
+        </div>
+    )
+}
+export default Footer;
---
+++ client/src/pages/Policy.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../components/LayOut/LayOut'
+
+
+const Policy = () => {
+  return (
+    <Layout>
+      <h1> Privacy Policy Page </h1>
+    </Layout>
+  )
+}
+
+export default Policy;
